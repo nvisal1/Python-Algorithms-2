58a5900bfacc4bacbdb36d9055533e2dd0822386
Nick Visalli <nvisal1@students.towson.edu>
2019-04-16 11:25:23 -0400

    write comment for josephus queue program

--- josephusQueue.py
+++ josephusQueue.py
@@ -1,15 +1,32 @@
+"""
+Name: Nicholas Visalli 
+Assignment number: 2
+Purpose: Implement Joesphus algorithm using queues
+"""
+
 import Queue as queue
 
+
+"""General user input
+"""
 count = raw_input("How many soldiers? ")
 input_string = raw_input("Enter " + count + " soldier names separated by a comma: ")
 soldier_list  = input_string.split(",")
 position = raw_input("Enter the position: ")
 
+
+"""Dequeue first `position` soldiers from queue
+"""
 q = queue.Queue(count)
 temp = queue.Queue(count)
 for soldier in soldier_list:
     q.put(soldier)
 
+"""Dequeue first `position - 1` soldiers from queue
+This removes the soldier at `position`
+
+Place remaing soldiers back into original queue
+"""
 while q.qsize() != 1:
     for i in range(int(position)):
         soldier = q.get()
