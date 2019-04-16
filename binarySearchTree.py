3df24b4efe819607b443add442dfd9a5ae07049e
Nick Visalli <nvisal1@students.towson.edu>
2019-04-16 11:22:34 -0400

    write comments for binary search tree

--- binarySearchTree.py
+++ binarySearchTree.py
@@ -1,14 +1,36 @@
+"""
+Name: Nicholas Visalli 
+Assignment number: 2
+Purpose: Generate a binary search tree and determine the average depth
+"""
+
 from random import *
 
 class Node: 
+    """Constructor for Node class
+    """
     def __init__(self,key): 
         self.left = None
         self.right = None
         self.val = key 
 
+    """Determine depth of given node
+    :param self: Node to determine depth of
+    :type self: Node
+    :returns: None 
+:   rtype: None
+    """
     def depth(self):
         return max(self.left.depth() if self.left else 0, self.right.depth() if self.right else 0) + 1
 
+"""Insert a node into the binary search tree
+:param root: root node of binary search tree
+:type root: Node
+:param node: node to insert into binary search tree
+:type node: Node
+:returns: None 
+:rtype: None
+"""
 def insert(root,node): 
     if root is None: 
         root = node 
@@ -24,6 +46,12 @@ def insert(root,node):
             else: 
                 insert(root.left, node) 
 
+"""Print binary search tree nodes inorder
+:param root: root node of binary search tree
+:type root: Node
+:returns: None 
+:rtype: None
+"""
 def inorder(root): 
     if root: 
         inorder(root.left) 
